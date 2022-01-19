0c752e965883bdc5783f2216cd4fe8fb53729b2b
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-01-19 22:24:27 +0100

    Shell script - Modularized and optimized 🔧

--- script.sh
+++ script.sh
@@ -1,8 +1,39 @@
 # COLOR VARIABLES
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m'
 YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+# FUNCTIONS
+
+function checkDB() {
+    if [ -z "$(mysql -u $dbuser -p$dbpass -e "SHOW DATABASES LIKE '$1'" | grep $1)" ]; then
+    echo "${RED}[ * ]${NC} Database ${BLUE}$1${NC} does not exist"
+    exit 1
+fi
+}
+function checkEmpty() {
+    if [ -z "$1" ]; then
+    echo "${RED}[ * ]${NC} Field cannot be empty"
+    exit 1
+fi
+}
+function checkTable() {
+    if [ -z "$(mysql -u $dbuser -p$dbpass -e "SHOW TABLES LIKE '$1'" | grep $1)" ]; then
+    echo "${RED}[ * ]${NC} Table ${BLUE}$1${NC} does not exist"
+    exit 1
+fi
+}
+
+function extractData() {
+    # If table is empty extract database else extract table
+    if [ -z $2 ]; then
+    mysqldump -u $dbuser -p$dbpass $1 > $(pwd)/$1.sql
+    else
+    mysqldump -u $dbuser -p$dbpass $1 $2 > $(pwd)/$2.sql
+    fi
+}
 
 echo "${GREEN}[ * ]${NC} DB Scheme and Data extractor"
 echo "\n"
@@ -11,55 +42,27 @@ echo "\n"
 
 sleep 1
 
-read -p "$(echo $YELLOW"[ ? ] "$NC) Enter database name: " dbname
-# Check if database name is not empty
-if [ -z "$dbname" ]; then
-    echo "${RED}[ * ]${NC} Database name cannot be empty"
-    echo "${RED}[ * ]${NC} Exiting..."
-    exit 1
-fi
-read -p "$(echo $YELLOW"[ ? ] "$NC) Enter database user: " dbuser
+read -p "$(echo $YELLOW"[ ? ] "$NC) Enter database user: "  dbuser 
 # Check if database user is not empty
-if [ -z "$dbuser" ]; then
-    echo "${RED}[ * ]${NC} Database user cannot be empty"
-    echo "${RED}[ * ]${NC} Exiting..."
-    exit 1
-fi
-read -p "$(echo $YELLOW"[ ? ] "$NC) Enter database password: " dbpass
+checkEmpty $dbuser
+read -s -p "$(echo $YELLOW"[ ? ] "$NC) Enter database password: " dbpass
+echo
 # Check if database password is not empty
-if [ -z "$dbpass" ]; then
-    echo "${RED}[ * ]${NC} Database password cannot be empty"
-    echo "${RED}[ * ]${NC} Exiting..."
-    exit 1
-fi
+checkEmpty $dbpass
+read  -p "$(echo $YELLOW"[ ? ] "$NC) Enter database name: " dbname
+# Check if database name is not empty
+checkEmpty $dbname
+checkDB $dbname
 read -p "$(echo $YELLOW"[ ? ] "$NC) Enter database table: " dbtable
 
 
-# Check if the database exists
-if [ -z "$(mysql -u $dbuser -p$dbpass -e "SHOW DATABASES LIKE '$dbname'" | grep $dbname)" ]; then
-    echo "${RED}[ * ]${NC} Database $dbname does not exist"
-    exit 1
-fi
-
-# Check if the table exists
-if [ -z "$(mysql -u $dbuser -p$dbpass -e "SHOW TABLES FROM $dbname LIKE '$dbtable'" | grep $dbtable)" ]; then
-    echo "${RED}[ * ]${NC} Table $dbtable does not exist"
-    exit 1
-fi
-
-if [$dbtable -eq ""] ; then
-    echo "${RED}[ * ]${NC} No table selected from $dbname"
-    echo "${GREEN}[ * ]${NC} Extracting entire database on $(pwd)/$dbname.sql"
-    mysqldump -u $dbuser -p$dbpass $dbname > $(pwd)/$dbname.sql
-else
-    echo "${GREEN}[ * ]${NC} Extracting table $dbtable from $dbname on $(pwd)/$dbtable.sql"
-    mysqldump -u $dbuser -p$dbpass $dbname $dbtable > $(pwd)/$dbtable.sql
-fi
+extractData $dbname $dbtable
 
 if [ $? -eq 0 ]; then
     echo "${GREEN}[ * ]${NC} Data extracted successfully!"
+    echo "${GREEN}[ * ]${NC} Check $(pwd)/$dbname.sql"
     exit 0
 else
-    echo "${RED}[ * ]${NC} Error while extracting data!"
+    echo "${RED}[ * ]${NC} Error while extracting data try again"
     exit 1
 fi
