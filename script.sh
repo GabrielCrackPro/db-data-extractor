48edc1aaf233b46b22fd6d27a60c1c97c7f89265
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-01-19 23:16:23 +0100

    Shell script - colors updated 🔧

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 # COLOR VARIABLES
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
+BLUE='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m'
 
@@ -9,19 +9,22 @@ NC='\033[0m'
 
 function checkDB() {
     if [ -z "$(mysql -u $dbuser -p$dbpass -e "SHOW DATABASES LIKE '$1'" 2>&1 | grep -v "Warning: Using a password" | grep $1)" ]; then
-    echo "${RED}[ * ]${NC} Database ${BLUE}$1${NC} does not exist"
+    echo "${RED}[ * ]${NC} Database ${RED}$1${NC} does not exist"
+    echo "${RED}[ * ]${NC} Exiting..."
     exit 1
 fi
 }
 function checkEmpty() {
     if [ -z "$1" ]; then
     echo "${RED}[ * ]${NC} Field cannot be empty"
+    echo "${RED}[ * ]${NC} Exiting..."
     exit 1
 fi
 }
 function checkTable() {
     if [ -z "$(mysql -u $dbuser -p$dbpass -e "SHOW TABLES LIKE '$1'" | grep $1)" ]; then
-    echo "${RED}[ * ]${NC} Table ${BLUE}$1${NC} does not exist"
+    echo "${RED}[ * ]${NC} Table ${RED}$1${NC} does not exist"
+    echo "${RED}[ * ]${NC} Exiting..."
     exit 1
 fi
 }
@@ -34,7 +37,6 @@ function extractData() {
     mysqldump -u $dbuser -p$dbpass $1 $2 2>&1 | grep -v "Warning: Using a password" > $(pwd)/$2.sql
     fi
 }
-
 echo "${GREEN}[ * ]${NC} DB Scheme and Data extractor"
 echo "\n"
 echo "${GREEN}[ * ]${NC} Starting..."
@@ -42,27 +44,27 @@ echo "\n"
 
 sleep 1
 
-read -p "$(echo $YELLOW"[ ? ] "$NC) Enter database user: "  dbuser 
+read -p "$(echo $BLUE"[ ? ] "$NC)Enter database $(echo $BLUE"user"$NC): "  dbuser 
 # Check if database user is not empty
 checkEmpty $dbuser
-read -s -p "$(echo $YELLOW"[ ? ] "$NC) Enter database password: " dbpass
+read -s -p "$(echo $BLUE"[ ? ] "$NC)Enter database $(echo $BLUE"password"$NC): " dbpass
 echo
 # Check if database password is not empty
 checkEmpty $dbpass
-read  -p "$(echo $YELLOW"[ ? ] "$NC) Enter database name: " dbname
+read  -p "$(echo $BLUE"[ ? ] "$NC)Enter database $(echo $BLUE"name"$NC): " dbname
 # Check if database name is not empty
 checkEmpty $dbname
 checkDB $dbname
-read -p "$(echo $YELLOW"[ ? ] "$NC) Enter database table: " dbtable
+read -p "$(echo $BLUE"[ ? ] "$NC)Enter database $(echo $BLUE"table"$NC): " dbtable
 
 
 extractData $dbname $dbtable
 
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ * ]${NC}  Data extracted successfully!"
-    echo "${GREEN}[ * ]${NC}  Check $(pwd)/$dbname.sql"
+    echo "${GREEN}[ * ]${NC}Data extracted successfully!"
+    echo "${GREEN}[ * ]${NC}Check $(pwd)/$dbname.sql"
     exit 0
 else
-    echo "${RED}[ * ]${NC} Error while extracting data try again"
+    echo "${RED}[ * ]${NC}Error while extracting data try again"
     exit 1
 fi
